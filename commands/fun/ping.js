d1dcda03310af8719fc83f05da204873183a7869
unknown <candrie2005@gmail.com>
2021-08-18 14:45:31 -0700

    ping command change

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -11,6 +11,6 @@ module.exports = {
     // Execute Command - Parameters: message
     execute(message) {
         // Send Message
-        message.channel.send('Poooooong!');
+        message.channel.send('Pooooooong!');
     },
 };
