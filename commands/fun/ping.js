a77917c97cffa89b9c6a97bd010a7518e5d97446
WoeTheBro <nazerlath.2005@gmail.com>
2021-08-19 11:46:14 -0700

    changed the ping to respond bruh with a cool looking emoji.

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,16 +1,21 @@
 // Ping Module
+
 module.exports = {
+    
     // Name of Command
     name: 'ping',
     // Description of Command
-    description: 'Ping!',
+    description: 'Ping Pong the battle of speeds!',
     // Guild - TRUE
     guildOnly: true,
     // Cooldown
-    cooldown: 50,
+    cooldown: 5,
     // Execute Command - Parameters: message
     execute(message) {
         // Send Message
-        message.channel.send('Pong!');
+        message.channel.send('bruh').then(sentMessage => {
+            // Add Reaction
+            sentMessage.react('😪');
+        });
     },
 };
